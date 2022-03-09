597d7e61c49cc5db7aabafeb72e0340cde039db2
Mansour Marvasti <mansour.marvasti@outlook.com>
2022-03-09 15:06:43 +0330

    add back worker listner for serial port

--- Communications/SerialCommunication.cs
+++ Communications/SerialCommunication.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Diagnostics;
 using System.IO.Ports;
+using System.Threading;
 
 namespace Communications
 {
@@ -15,25 +16,42 @@ namespace Communications
             string[]? ports = SerialPort.GetPortNames();
             _port = new SerialPort();
             if (ports.Length > 0)
-                _port.PortName = ports[0];
-            _port.BaudRate = 9600;
+                _port.PortName = "COM3";//ports[0];
+            _port.BaudRate = 115200;
+
+            Thread trd = new Thread(backWork);
+            trd.IsBackground = true;
+            trd.Start();
         }
-        public SerialComminucation(string portName, int buad, Action dataRecieved)
+        public SerialComminucation(string portName, int buad)
         {
-            _port = new SerialPort(portName, buad);
+            _port = new SerialPort(portName, buad)
+            {
+                ReadTimeout = 1000,
+                WriteTimeout = 1000
+            };
 
-            _port.ReadTimeout = 1000;
-            _port.WriteTimeout = 1000;
+            //_port.DataReceived += new SerialDataReceivedEventHandler(_port_DataReceived);
 
-            _port.DataReceived += _port_DataReceived;
+            Thread trd = new Thread(backWork);
+            trd.IsBackground = true;
+            trd.Start();
         }
 
-        private void _port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        //private void _port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        //{
+        //    string msg = Read();
+        //    MessageReceived?.Invoke(this, msg);
+        //}
+
+        private void backWork()
         {
-            string msg = Read();
-            MessageReceived?.Invoke(this, msg);
+            while (true)
+            {
+                string msg = Read();
+                MessageReceived?.Invoke(this, msg);
+            }
         }
-
         public bool IsConnected => _port.IsOpen;
 
         public bool Connect()
@@ -62,8 +80,20 @@ namespace Communications
                     return "";
                 }
             }
+            try
+            {
+                return _port.ReadLine();
 
-            return _port.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return "";
+            }
+            finally
+            {
+
+            }
         }
 
         public void Write(string data)
@@ -74,6 +104,7 @@ namespace Communications
             }
 
             _port.WriteLine(data);
+
         }
 
         public void ChangeSetting(ICommunicationSetting setting)
@@ -81,10 +112,11 @@ namespace Communications
             try
             {
                 SerialSetting st = (SerialSetting)setting;
+                _port.Close();
                 _port.BaudRate = st.Baudrate;
                 _port.PortName = st.PortName;
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Debug.WriteLine(ex.Message);
             }
