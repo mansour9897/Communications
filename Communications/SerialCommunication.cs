f9e3d2ae8f9102a8d9098e77a9667b95d05ce462
mansour9897 <mansour9897@gmail.com>
2023-05-17 05:32:38 +0330

    correct serial recive and change setting

--- Communications/SerialCommunication.cs
+++ Communications/SerialCommunication.cs
@@ -1,148 +1,156 @@
-using System;
 using System.Diagnostics;
 using System.IO.Ports;
-using System.Threading;
 
 namespace Communications
 {
-    public class SerialComminucation : ICommunication
-    {
-        private readonly SerialPort _port;
+	public class SerialComminucation : ICommunication
+	{
+		private readonly SerialPort _port;
 
-        public event ICommunication.MessageReceivedHandler? MessageReceived;
+		public event ICommunication.MessageReceivedHandler? MessageReceived;
 
-        public SerialComminucation()
-        {
-            string[]? ports = SerialPort.GetPortNames();
-            _port = new SerialPort();
-            if (ports.Length > 0)
-                _port.PortName = ports[0];
-            _port.BaudRate = 115200;
+		public SerialComminucation()
+		{
+			string[]? ports = SerialPort.GetPortNames();
+			_port = new SerialPort();
+			if (ports.Length > 0)
+				_port.PortName = ports[0];
+			_port.BaudRate = 115200;
+			_port.DtrEnable = true;
 
-            Thread trd = new Thread(backWork);
-            trd.IsBackground = true;
-            trd.Start();
-        }
-        public SerialComminucation(string portName, int buad)
-        {
-            _port = new SerialPort(portName, buad)
-            {
-                ReadTimeout = 1000,
-                WriteTimeout = 1000
-            };
-            _port.DtrEnable = true;
-            _port.RtsEnable = true;
-            _port.DataReceived += new SerialDataReceivedEventHandler(_port_DataReceived);
+			_port.DataReceived += new SerialDataReceivedEventHandler(_port_DataReceived);
 
-            Thread trd = new Thread(backWork);
-            trd.IsBackground = true;
-            //trd.Start();
-        }
+		}
+		public SerialComminucation(string portName, int buad)
+		{
+			_port = new SerialPort(portName, buad)
+			{
+				ReadTimeout = 1000,
+				WriteTimeout = 1000
+			};
+			_port.DtrEnable = true;
+			_port.RtsEnable = true;
+			_port.DataReceived += new SerialDataReceivedEventHandler(_port_DataReceived);
 
-        private void _port_DataReceived(object sender, SerialDataReceivedEventArgs e)
-        {
-            Thread.Sleep(10);
-            string msg = Read();
-            // Console.WriteLine("Received: " + msg);
-            Debug.WriteLine("Received: " + msg);
-            MessageReceived?.Invoke(this, msg);
-        }
+			//Thread trd = new Thread(backWork);
+			//trd.IsBackground = true;
+			////trd.Start();
+		}
 
-        private void backWork()
-        {
-            while (true)
-            {
-                string msg = "!00";//Read();
-                MessageReceived?.Invoke(this, msg);
-            }
-        }
-        public bool IsConnected => _port.IsOpen;
+		private void _port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+		{
+			Thread.Sleep(10);
+			string msg = Read();
+			// Console.WriteLine("Received: " + msg);
+			Debug.WriteLine("Received: " + msg);
+			MessageReceived?.Invoke(this, msg);
+		}
 
-        public bool Connect()
-        {
-            if (_port.IsOpen) return true;
+		//private void backWork()
+		//{
+		//    while (true)
+		//    {
+		//        string msg = "!00";//Read();
+		//        MessageReceived?.Invoke(this, msg);
+		//    }
+		//}
+		public bool IsConnected => _port.IsOpen;
 
-            try
-            {
-                _port.Open();
-                return true;
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex.Message);
-                return false;
-            }
-            finally
-            {
-            }
-        }
+		public bool Connect()
+		{
+			if (_port.IsOpen) return true;
 
-        public string Read()
-        {
-            if (_port.IsOpen == false)
-            {
-                if (!Connect())
-                {
-                    return "";
-                }
-            }
-            try
-            {
-                string msg = _port.ReadLine();
-                Debug.WriteLine("Read: " + msg);
-                return msg.Trim();
+			try
+			{
+				_port.Open();
+				return true;
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex.Message);
+				return false;
+			}
+			finally
+			{
+			}
+		}
 
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex.Message);
-                return "";
-            }
-            finally
-            {
+		public string Read()
+		{
+			if (_port.IsOpen == false)
+			{
+				if (!Connect())
+				{
+					return "";
+				}
+			}
+			try
+			{
+				string msg = _port.ReadLine();
+				Debug.WriteLine("Read: " + msg);
+				return msg.Trim();
 
-            }
-        }
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex.Message);
+				return "";
+			}
+			finally
+			{
 
-        public void Write(string data)
-        {
-            if (_port.IsOpen == false)
-            {
-                if (!Connect()) return;
-                Debug.WriteLine("Connected");
-            }
+			}
+		}
 
-            try
-            {
-                _port.Write(data);
-                Debug.WriteLine("Sent: " + data);
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex.Message);
-            }
-            finally
-            {
+		public void Write(string data)
+		{
+			if (_port.IsOpen == false)
+			{
+				if (!Connect()) return;
+				Debug.WriteLine("Connected");
+			}
 
-            }
-            //_port.WriteLine(data);
+			try
+			{
+				_port.Write(data);
+				Debug.WriteLine("Sent: " + data);
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex.Message);
+			}
+			finally
+			{
 
-        }
+			}
+			//_port.WriteLine(data);
 
-        public void ChangeSetting(ICommunicationSetting setting)
-        {
-            try
-            {
-                SerialSetting st = (SerialSetting)setting;
-                _port.Close();
-                _port.BaudRate = st.Baudrate;
-                _port.PortName = st.PortName;
-                _port.DataReceived += new SerialDataReceivedEventHandler(_port_DataReceived);
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex.Message);
-            }
-        }
-    }
+		}
+
+		public void ChangeSetting(ICommunicationSetting setting)
+		{
+			try
+			{
+				SerialSetting st = (SerialSetting)setting;
+				_port.Close();
+
+				if (st.PortName is not null)
+					_port.PortName = st.PortName;
+
+				_port.BaudRate = st.BaudRate;
+				_port.Parity = st.Parity;
+				_port.StopBits = st.StopBits;
+				_port.DataBits = st.DataBits;
+				_port.Handshake = st.Handshake;
+				_port.DtrEnable = true;
+
+
+				_port.DataReceived += new SerialDataReceivedEventHandler(_port_DataReceived);
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex.Message);
+			}
+		}
+	}
 }
--- Communications/SerialSetting.cs
+++ Communications/SerialSetting.cs
@@ -1,8 +1,14 @@
-﻿namespace Communications
+﻿using System.IO.Ports;
+
+namespace Communications
 {
     public class SerialSetting : ICommunicationSetting
     {
-        public string PortName { get; set; }
-        public int Baudrate { get; set; }
-    }
+		public string? PortName { get; set; }
+		public int BaudRate { get; set; }
+		public int DataBits { get; set; }
+		public Parity Parity { get; set; }
+		public StopBits StopBits { get; set; }
+		public Handshake Handshake { get; set; }
+	}
 }
